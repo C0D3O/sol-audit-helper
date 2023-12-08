4008e9c97557bacc9c44dc4d822f46ada834c14b
moneyDev1111 <1111eugene.eugene@gmail.com>
2023-12-08 10:53:10 +0200

    upd

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { appendFileSync, readdirSync, writeFileSync } from 'fs';
+import sloc from 'node-sloc';
 
 export const getFolders = (source: string) =>
 	readdirSync(source, { withFileTypes: true })
@@ -210,14 +211,17 @@ export const htmlTemplate = `
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <style>
+		:root {
+			--font-color: rgb(213, 213, 213);
+		}
 		* {
 			box-sizing: border-box;
 			margin: 0;
 		}
 		
 		body {
-			background: rgb(19, 47, 50);
-			background: linear-gradient(18deg, rgba(19, 47, 50, 1) -50%, rgba(120, 247, 255, 1) 150%);
+			background: rgb(35, 41, 20);
+			background: linear-gradient(8deg, rgb(18, 21, 12) 0%, rgb(42, 39, 30) 300%);
 			background-repeat: no-repeat;
 			height: 100vh;
 		}
@@ -227,30 +231,44 @@ export const htmlTemplate = `
 		}
 		
 		table {
+			background-color: rgb(79, 75, 69);
 			text-align: center;
 			border: 3px solid black !important;
 			border-radius: 10px;
 			padding: 0.4em;
 			border-collapse: separate;
+		
+			box-shadow: 5px 10px 30px rgba(168, 164, 164, 0.2);
 		}
 		
 		tr > td {
-			color: aliceblue !important;
+			color: var(--font-color) !important;
 			font-size: 1.3rem;
 		}
 		th {
 			border-style: hidden !important;
+			color: rgb(120, 120, 121);
+			font-weight: 300;
 		}
 		tr:last-child > td {
 			border-style: hidden !important;
 		}
 		select {
+			margin: 0 auto;
+			width: 75% !important;
 			text-align: center;
 		}
 		.form-select {
-			background-color: #9f9d9d;
-			color: aliceblue;
+			background-color: #757474;
+			color: var(--font-color);
+			border: none;
 		}
+		.sLocColumn:hover {
+			cursor: pointer;
+		}		
+		.statusColumn:hover {
+			cursor: pointer;
+		}		
 	    </style>
 
     </head>
@@ -261,9 +279,146 @@ export const htmlTemplate = `
                 <thead>
                     <tr>
                         <th scope="col">File</th>
-                        <th scope="col" onclick="sortTable()">sLoc</th>
-                        <th scope="col">Status</th>
+                        <th class="sLocColumn" scope="col" onclick="sortTable()">sLoc</th>
+                        <th class="statusColumn" scope="col" onclick="sortTableByStatus()">Status</th>
                     </tr>
                 </thead>
                 <tbody>
 `;
+
+export const generateSlocReport = async (cwd: string, scopeNames: string[], newPath: string) => {
+	// CREATE A SLOC REPORT
+
+	writeFileSync(`${cwd}/sLoc.html`, htmlTemplate);
+	let counter = 0;
+	for await (let scopeFileName of scopeNames) {
+		try {
+			let id = 'a' + counter;
+			counter++;
+
+			const sLocOutput = await sloc({
+				path: `${newPath}${scopeFileName}`,
+				extensions: ['sol'],
+			});
+
+			let fileName = sLocOutput?.paths[0].split('/').pop()?.slice(0, -4);
+			let sLocNumber = sLocOutput?.sloc;
+
+			const DataToAppend = `
+			<tr>
+				<td>${fileName}</td>
+				<td>${sLocNumber}</td>
+				<td>
+					<select id="${id}" class="form-select" aria-label="Default select example"
+						onchange="changeFunc(this)">
+						<option value="done">Done</option>
+						<option value="in_progress">In Progress</option>
+						<option value="not_started" selected>Not Started</option>
+					</select>
+				</td>
+			</tr>`;
+			appendFileSync(`${cwd}/sLoc.html`, DataToAppend);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	const dataToWrapTheHtmlWith = `
+		</tbody>
+				</table>
+			</div>
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+				integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+				crossorigin="anonymous"></script>
+				<script>
+				let ascending = true;
+
+				const sortTable = () => {
+					let table, rows, switching, i, x, y, shouldSwitch;
+					table = document.querySelector('table');
+					switching = true;
+
+					while (switching) {
+						switching = false;
+						rows = table.rows;
+
+						for (i = 1; i < rows.length - 1; i++) {
+							shouldSwitch = false;
+							x = parseInt(rows[i].getElementsByTagName('td')[1].innerHTML);
+							y = parseInt(rows[i + 1].getElementsByTagName('td')[1].innerHTML);
+
+							if (ascending ? x > y : x < y) {
+								shouldSwitch = true;
+								break;
+							}
+						}
+
+						if (shouldSwitch) {
+							rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+							switching = true;
+						}
+					}
+					
+					// Toggle sorting direction for the next function call
+					ascending = !ascending;
+				};
+
+				let statusAscending = true;
+				const sortTableByStatus = () => {
+					let table, rows, switching, i, x, y, shouldSwitch;
+					table = document.querySelector('table');
+					switching = true;
+
+					while (switching) {
+						switching = false;
+						rows = table.rows;
+
+						for (i = 1; i < rows.length - 1; i++) {
+							shouldSwitch = false;
+							x = rows[i].getElementsByTagName('td')[2].querySelector('select').value;
+							y = rows[i + 1].getElementsByTagName('td')[2].querySelector('select').value;
+
+							if (statusAscending ? x.localeCompare(y) > 0 : x.localeCompare(y) < 0) {
+								shouldSwitch = true;
+								break;
+							}
+						}
+
+						if (shouldSwitch) {
+							rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+							switching = true;
+						}
+					}
+
+					// Toggle sorting direction for the next function call
+					statusAscending = !statusAscending;
+				};
+
+		
+				const changeFunc = (selectObject) => {
+					const value = selectObject.value;
+					window.sessionStorage.setItem(selectObject.id, value);
+				};
+		
+				window.onload = () => {
+					sortTable();
+		
+					Object.keys(sessionStorage).forEach(function (selector) {
+						const value = sessionStorage.getItem(selector);
+		
+						document
+							.querySelector('#' + selector)
+							.querySelectorAll('option')
+							.forEach((option) => {
+								if (option.value === value) {
+									console.log('true');
+									option.selected = true;
+								}
+							});
+					});
+				};
+			</script>
+		</body>
+
+	</html>`;
+	appendFileSync(`${cwd}/sLoc.html`, dataToWrapTheHtmlWith);
+};
